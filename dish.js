737e612c57c2a43ef582b01f4433190af5ecfe83
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-02-12 10:33:24 +0100

    risultato buono accettabile les oski

--- dish.js
+++ dish.js
@@ -4,18 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (selectedDish) {
         const dishContent = `
-            <h2>${selectedDish.name}</h2>
-            <img src="assets/${selectedDish.image}" alt="${selectedDish.name}" class="img-fluid mb-3">
-            <p>${selectedDish.description}</p>
-            <p><strong>Precio: $${selectedDish.price.toFixed(2)}</strong></p>
+            <div class="card mx-auto" style="width: 24rem;">
+                <img src="assets/${selectedDish.image}" class="card-img-top" alt="${selectedDish.name}">
+                <div class="card-body text-center">
+                    <h2 class="card-title">${selectedDish.name}</h2>
+                    <p class="card-text">${selectedDish.description}</p>
+                    <p class="card-text"><strong>Precio: $${selectedDish.price.toFixed(2)}</strong></p>
+                    <button id="back-button" class="btn btn-primary mt-3">Torna indietro</button>
+                </div>
+            </div>
         `;
         dishDetail.innerHTML = dishContent;
+
+        const backButton = document.getElementById('back-button');
+        backButton.addEventListener('click', () => {
+            window.location.href = 'index.html';
+        });
     } else {
         dishDetail.innerHTML = '<p>No se ha seleccionado ningún plato.</p>';
     }
-
-    const backButton = document.getElementById('back-button');
-    backButton.addEventListener('click', () => {
-        window.location.href = 'index.html';
-    });
 });
